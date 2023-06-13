dc2b92eb7f3a0815b6c673fc30dd652b81945067
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-13 18:33:26 +0900

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by 飯塚政美 on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
